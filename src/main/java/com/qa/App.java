81867e1529560ce13f4b2e503f5c1df31a709a15
unknown <Dan>
2026-04-28 16:15:09 +0100

    EDIT New change and release version

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Dan"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Stephen"));
         System.out.println(sayGoodbye());
     }
 
